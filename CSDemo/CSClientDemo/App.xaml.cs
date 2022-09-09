8ccf259bedd1969687f008d7adfd3376e8d68a7c
chenshenchao <chenshenchao@outlook.com>
2022-09-09 17:29:49 +0800

    save

--- CSDemo/CSClientDemo/App.xaml.cs
+++ CSDemo/CSClientDemo/App.xaml.cs
@@ -1,17 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
 using System.Windows;
+using Serilog;
+using Serilog.Events;
 
-namespace CSClientDemo
+namespace CSClientDemo;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public IServiceProvider ServiceProvider { get; private set; } = null!;
+    public IConfiguration Configuration { get; private set; } = null!;
+
+    protected override void OnStartup(StartupEventArgs e)
     {
+        base.OnStartup(e);
+        var builder = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+        Configuration = builder.Build();
+
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddSingleton<ILogger>(op =>
+         {
+             var path = Configuration.GetRequiredSection("Logger:PathFormat");
+             return new LoggerConfiguration()
+                 .MinimumLevel.Information()
+                 .WriteTo.File(
+                         path: path?.ToString() ?? "Logs/S-{Date}.log",
+                         rollingInterval: RollingInterval.Day,
+                         rollOnFileSizeLimit: true,
+                         fileSizeLimitBytes: int.Parse(Configuration.GetSection("Logger:FileSizeLimitBytes")?.Value ?? "2000000"),
+                         retainedFileCountLimit: int.Parse(Configuration.GetSection("Logger:RetainedFileCountLimit")?.Value ?? "31"),
+                         flushToDiskInterval: TimeSpan.FromSeconds(10),
+                         outputTemplate: Configuration.GetSection("Logger:OutputTemplate")?.Value ?? "[{Timestamp:yy-MM-dd HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}"
+                     )
+                     .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Information)
+                     .CreateLogger();
+         });
+
+        // serviceCollection.Configure<AppSettings>(Configuration.GetSection(nameof(AppSettings)));
+        serviceCollection.AddTransient(typeof(MainWindow));
+        //ConfigureServices(serviceCollection);
+
+        ServiceProvider = serviceCollection.BuildServiceProvider();
+
+        var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+        mainWindow.Show();
     }
+
 }
