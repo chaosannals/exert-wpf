4d1339d8d876a383500ec97ef9a2c1b8d40f1412
chenshenchao <chenshenchao@outlook.com>
2023-03-20 02:10:03 +0800

    save

--- DiDemo/DiDemo/App.xaml.cs
+++ DiDemo/DiDemo/App.xaml.cs
@@ -8,58 +8,57 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace DiDemo
+namespace DiDemo;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    private ILogger<App> logger;
+
+    public IHost AppHost { get; init; }
+
+    public App()
     {
-        private ILogger<App> logger;
+        AppHost = Host.CreateDefaultBuilder()
+            .ConfigureHostConfiguration(cd =>
+            {
+                cd.SetBasePath(Directory.GetCurrentDirectory());
+                cd.AddJsonFile("appsettings.Development.json", optional: true);
+                cd.AddEnvironmentVariables(prefix: "DI_DEMO_");
+            })
+            .ConfigureLogging((hc, cl) =>
+            {
+                cl.AddFile(hc.Configuration.GetSection("LoggingFile"));
+                cl.AddConsole();
+            })
+            .ConfigureServices((hc, services) =>
+            {
+                services.AddSingleton(_ => this);
+                services.AddSingleton<MainWindow>();
+                services.AddHostedService<TcpService>();
+            })
+            .Build();
+        logger = AppHost.Services.GetRequiredService<ILogger<App>>();
+        logger.LogInformation("scheduler: {0}", TaskScheduler.Current);
+        MainWindow = AppHost.Services.GetRequiredService<MainWindow>();
+    }
 
-        public IHost AppHost { get; init; }
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+        logger.LogInformation("startup begin in: {0}", Thread.CurrentThread.ManagedThreadId);
+        await AppHost.StartAsync();
+        MainWindow.Show();
+        logger.LogInformation("startup end in: {0}", Thread.CurrentThread.ManagedThreadId);
+        base.OnStartup(e);
+    }
 
-        public App()
-        {
-            AppHost = Host.CreateDefaultBuilder()
-                .ConfigureHostConfiguration(cd =>
-                {
-                    cd.SetBasePath(Directory.GetCurrentDirectory());
-                    cd.AddJsonFile("appsettings.Development.json", optional: true);
-                    cd.AddEnvironmentVariables(prefix: "DI_DEMO_");
-                })
-                .ConfigureLogging((hc, cl) =>
-                {
-                    cl.AddFile(hc.Configuration.GetSection("LoggingFile"));
-                    cl.AddConsole();
-                })
-                .ConfigureServices((hc, services) =>
-                {
-                    services.AddSingleton(_ => this);
-                    services.AddSingleton<MainWindow>();
-                    services.AddHostedService<TcpService>();
-                })
-                .Build();
-            logger = AppHost.Services.GetRequiredService<ILogger<App>>();
-            logger.LogInformation("scheduler: {0}", TaskScheduler.Current);
-            MainWindow = AppHost.Services.GetRequiredService<MainWindow>();
-        }
-
-        protected override async void OnStartup(StartupEventArgs e)
-        {
-            logger.LogInformation("startup begin in: {0}", Thread.CurrentThread.ManagedThreadId);
-            await AppHost.StartAsync();
-            MainWindow.Show();
-            logger.LogInformation("startup end in: {0}", Thread.CurrentThread.ManagedThreadId);
-            base.OnStartup(e);
-        }
-
-        protected override async void OnExit(ExitEventArgs e)
-        {
-            logger.LogInformation("exit begin in: {0}", Thread.CurrentThread.ManagedThreadId);
-            await AppHost.StopAsync();
-            logger.LogInformation("exit end in: {0}", Thread.CurrentThread.ManagedThreadId);
-            base.OnExit(e);
-        }
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        logger.LogInformation("exit begin in: {0}", Thread.CurrentThread.ManagedThreadId);
+        await AppHost.StopAsync();
+        logger.LogInformation("exit end in: {0}", Thread.CurrentThread.ManagedThreadId);
+        base.OnExit(e);
     }
 }
