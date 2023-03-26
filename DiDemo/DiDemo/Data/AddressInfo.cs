d678c819ae076e675d7d6906af83a2ca5eea1ffe
chenshenchao <chenshenchao@outlook.com>
2023-03-26 23:59:18 +0800

    save

---
+++ DiDemo/DiDemo/Data/AddressInfo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiDemo.Data;
+
+public class AddressInfo
+{
+    public string StreetName { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+}
+
+public class AddressList : ObservableCollection<AddressInfo>
+{
+    public AddressList(): base()
+    {
+        Add(new AddressInfo
+        {
+            Country = "中国",
+            City = "汕头",
+            StreetName = "街道1",
+        });
+        Add(new AddressInfo
+        {
+            Country = "中国",
+            City = "汕头",
+            StreetName = "街道2",
+        });
+        Add(new AddressInfo
+        {
+            Country = "中国",
+            City = "汕头",
+            StreetName = "街道3",
+        });
+        Add(new AddressInfo
+        {
+            Country = "中国",
+            City = "汕头",
+            StreetName = "街道4",
+        });
+        Add(new AddressInfo
+        {
+            Country = "中国",
+            City = "汕头",
+            StreetName = "街道5",
+        });
+    }
+}
---
+++ DiDemo/DiDemo/Views/ButtonTemplatePage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace DiDemo.Views
+{
+    /// <summary>
+    /// ButtonTemplatePage.xaml 的交互逻辑
+    /// </summary>
+    public partial class ButtonTemplatePage : Page
+    {
+        public ButtonTemplatePage()
+        {
+            InitializeComponent();
+            ReadDefaultButtonTemplate();
+        }
+
+        private void ReadDefaultButtonTemplate()
+        {
+            var button = new Button();
+            button.Content = "test";
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+            settings.IndentChars = new string(' ', 4);
+            settings.NewLineOnAttributes = true;
+            using var stream = new MemoryStream();
+            var writer = XmlWriter.Create(stream, settings);
+            XamlWriter.Save(button.Template, writer);
+            stream.Position = 0;
+            TextPanel.Text = new StreamReader(stream).ReadToEnd();
+        }
+    }
+}
