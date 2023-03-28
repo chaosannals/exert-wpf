7eae6d0aeafddcdca9580aabca714e98da48f79a
chenshenchao <chenshenchao@outlook.com>
2023-03-29 01:15:34 +0800

    save

---
+++ DiDemo/DiDemo/Data/StateHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiDemo.Data;
+
+public static class StateHelper
+{
+    public static IEnumerable<StateInfo> GetStates()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            yield return new StateInfo(i + 100, $"State-{i}");
+        }
+    }
+}
---
+++ DiDemo/DiDemo/Data/StateInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiDemo.Data;
+
+public class StateInfo
+{
+    public int Id { get; init; }
+    public string Name { get; init; }
+    public string Description { get; set; }
+
+    public StateInfo(int id, string name)
+    {
+        Id = id;
+        Name = name;
+        Description = name;
+    }
+}
